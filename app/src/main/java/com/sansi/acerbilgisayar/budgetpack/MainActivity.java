64b567624f6857556aa8fb36320cb3809043b4b4
anilsansak <anilsansak@gmail.com>
2017-07-20 11:12:37 +0300

    intro slider added

--- app/src/main/java/com/sansi/acerbilgisayar/budgetpack/MainActivity.java
+++ app/src/main/java/com/sansi/acerbilgisayar/budgetpack/MainActivity.java
@@ -2,12 +2,21 @@ package com.sansi.acerbilgisayar.budgetpack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView welcomeText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        welcomeText = (TextView) findViewById(R.id.welcomeTxt);
+
+        welcomeText.setText("Welcome to Budgetpack!");
+
     }
 }
