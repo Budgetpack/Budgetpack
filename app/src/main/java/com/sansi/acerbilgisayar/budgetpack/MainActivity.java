aa85fa0463e5969944076d60a0f75eef457f9032
anilsansak <anilsansak@gmail.com>
2017-07-20 11:28:29 +0300

    actionbar removed

--- app/src/main/java/com/sansi/acerbilgisayar/budgetpack/MainActivity.java
+++ app/src/main/java/com/sansi/acerbilgisayar/budgetpack/MainActivity.java
@@ -2,6 +2,7 @@ package com.sansi.acerbilgisayar.budgetpack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
